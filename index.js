7c4de637d456ca179c3493aad2cf5b469621666a
yifan <yifan@yifans-iMac.local>
2015-10-07 12:05:20 +0800

    Stop runtime when process exits

--- index.js
+++ index.js
@@ -116,3 +116,20 @@ makeGaia.run(function () {
   }
   watch();
 });
+
+function exitHandler(options, err) {
+  runtime.stop();
+
+  if (options.cleanup) console.log('clean');
+  if (err) console.log(err.stack);
+  if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
